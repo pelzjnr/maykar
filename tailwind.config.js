99e04e2ea4371bfda11ff61d5ef4432d9888ec93
pelzjnr <pelzjnr@github.com>
2026-02-08 05:49:14 -0600

    restore orange #ff4d00

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,6 @@ module.exports = {
       colors: {
         orange: {
           DEFAULT: '#ff4d00',
-          50: '#fff7f0',
-          100: '#ffe8d6',
-          200: '#ffd0ad',
-          300: '#ffb074',
-          400: '#ff8842',
-          500: '#ff4d00',
-          600: '#e63c00',
-          700: '#bf2e00',
-          800: '#992400',
-          900: '#7a1d00',
         },
       },
       fontFamily: {
